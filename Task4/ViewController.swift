5cb2b7c33df9df3f056adb42bd2b5c4c6a0d890d
Dev 2 <devios2@devs-imac-7.local>
2019-05-28 11:06:51 +0700

    Initial Commit

---
+++ Task4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task4
+//
+//  Created by Dev iOS 2 on 5/28/19.
+//  Copyright © 2019 Dev iOS 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
